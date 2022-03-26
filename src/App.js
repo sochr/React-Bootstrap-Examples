28a72767ec324a8d730b8151175c22d6d6765279
sochr <102359627+sochr@users.noreply.github.com>
2022-03-26 16:17:36 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import { LinkContainer } from "react-router-bootstrap";
 
 import "./App.css";
+import { Navbar } from "react-bootstrap";
 
 const Home = () => <span>Home</span>;
 
@@ -16,8 +17,27 @@ const Users = () => <span>Users</span>;
 
 const App = () => (
   <MemoryRouter>
+    <Navbar className="">
+      <Container>
+        <h2>
+          Navigate to{" "}
+          <ButtonToolbar className="custom-btn-toolbar">
+            <LinkContainer to="/">
+              <Button>Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Button>About</Button>
+            </LinkContainer>
+            <LinkContainer to="/users">
+              <Button>Users</Button>
+            </LinkContainer>
+          </ButtonToolbar>
+        </h2>
+      </Container>
+    </Navbar>
+
     <Container className="p-3">
-      <Container className="p-4 mb-4 bg-light rounded-3">
+      <Container className="p-5 mb-4 bg-light rounded-3">
         <h1 className="header">Welcome To React-Bootstrap</h1>
         <h2>
           Current Page is{" "}
@@ -32,20 +52,6 @@ const App = () => (
               <Home />
             </Route>
           </Switch>
-        </h2>
-        <h2>
-          Navigate to{" "}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
         </h2>
       </Container>
     </Container>
